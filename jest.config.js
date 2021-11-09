9240dcd0f420dd11420ae9281db6a5121eb897f4
Bob Olde Hampsink <bob@robuust.digital>
2021-11-09 08:36:51 +0100

    Reproduce issue

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest'
   },
-  transformIgnorePatterns: ['/node_modules/(?!@ionic/vue|@ionic/vue-router)']
+  transformIgnorePatterns: ['/node_modules/(?!@ionic/core|@ionic/vue|@ionic/vue-router|@stencil/core|ionicons)']
 }
